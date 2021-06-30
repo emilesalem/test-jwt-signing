83504c0f368ae1c6751124a3c570f8aa625e1175
emile <emile.salem@aspentech.com>
2021-06-30 11:33:03 -0400

    ongoing

--- go/test.go
+++ go/test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"time"
@@ -15,45 +16,113 @@ func sign() {
 }
 
 func main() {
+
+	// duration of the test
+	elapsed := 10 * time.Millisecond
+
+	deadline := time.Now().Add(elapsed)
+
 	// number of signing goroutines
 	workerPool := 1
 
 	// rate of signing requests per milliseconds
-	milliRate := 500
+	milliRate := 1
 
-	// duration of the test
-	elapsed := 10 * time.Second
-
-	workerChan := make(chan struct{})
+	workerChan := make(chan chan struct{})
 
 	signed := make(chan struct{})
 
+	available := make(chan struct{})
+
+	dispatch := make(chan string)
+
 	totalSigned := 0
+
+	mainCtx, cancel := context.WithDeadline(context.Background(), deadline)
+	defer cancel()
+
+	dispatchCtx, dispatchCancel := context.WithDeadline(mainCtx, deadline)
 	go func() {
-		for range signed {
-			totalSigned++
+		defer dispatchCancel()
+		requestNb := 1
+		workChanNb := 0
+		for {
+			select {
+			case x := <-workerChan:
+				go func() {
+					for {
+						select {
+						case <-x:
+							<-available
+							dispatch <- fmt.Sprintf(`workchan: %v requestNb: %v`, workChanNb, requestNb)
+							requestNb++
+						case <-dispatchCtx.Done():
+							return
+						}
+					}
+				}()
+				workChanNb++
+			case <-dispatchCtx.Done():
+				println(`done dispatching`)
+				return
+			}
 		}
 	}()
+
+	counterCtx, counterCancel := context.WithDeadline(mainCtx, deadline)
+	go func() {
+		defer counterCancel()
+		for {
+			select {
+			case <-signed:
+				totalSigned++
+			case <-counterCtx.Done():
+				println(`we're done`)
+				cancel()
+				return
+			}
+		}
+	}()
+
 	for i := 1; i <= workerPool; i++ {
+		wCtx, wCancel := context.WithDeadline(mainCtx, deadline)
 		go func() {
-			for range workerChan {
-				sign()
-				signed <- struct{}{}
+			defer wCancel()
+			available <- struct{}{}
+			for {
+				select {
+				case x := <-dispatch:
+					fmt.Printf("signing request %v\n", x)
+					sign()
+					signed <- struct{}{}
+					available <- struct{}{}
+				case <-wCtx.Done():
+					close(available)
+					close(signed)
+					println(`we're done 2`)
+					return
+				}
 			}
 		}()
 	}
+
 	totalSent := 0
-	end := time.After(elapsed)
 	for range time.Tick(1 * time.Millisecond) {
 		select {
-		case <-end:
+		case <-mainCtx.Done():
+			close(workerChan)
 			fmt.Printf("signed %v times in %v seconds, requested %v signatures\n", totalSigned, elapsed, totalSent)
 			os.Exit(0)
 		default:
-			for i := 0; i < milliRate; i++ {
-				totalSent++
-				workerChan <- struct{}{}
-			}
+			workChan := make(chan struct{})
+			workerChan <- workChan
+			go func() {
+				for i := 0; i < milliRate; i++ {
+					totalSent++
+					workChan <- struct{}{}
+				}
+				close(workChan)
+			}()
 		}
 	}
 }
